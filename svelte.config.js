5d791ba44791a9d86893c02f79497eb4489c3d2a
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-06-14 17:43:25 +0300

    config

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter(),
+    prerender: {
+      default: true
+    },
     methodOverride: {
       allowed: ['PATCH', 'DELETE'],
     },
