3e80c8a7fb3a37f929ab110f538a773f06c72ac4
saadeghi <pouya.saadeghi@gmail.com>
2022-06-14 15:01:50 +0000

    Deploy https://github.com/saadeghi/saadeghi.github.io/commit/6b73a59b5bab1310f4a084ce1d9653bc202e5fa9

--- src/routes/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import '../app.css';
-</script>
-<svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700;800;900&display=swap" rel="stylesheet">
-</svelte:head>
-<slot />
