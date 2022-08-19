5a23192c2f4f8e570b7fbed63c2850421f1d31e3
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-08-19 07:11:29 +0300

    disable sveltekit hydrate

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter(),
+    browser: {
+      hydrate: false,
+      router: false,
+    },
     prerender: {
       default: true
     },
