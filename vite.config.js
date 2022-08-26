2fcdbcc48fff99a91108c6c853f7027697366d33
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2022-08-27 02:21:32 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite'
 import path from 'path'
 
-
 export default defineConfig({
   plugins: [sveltekit()],
   resolve:{
